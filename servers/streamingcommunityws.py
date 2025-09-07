288f3e1c183e6eda8348e1fa8a182e6196bddb63
ilmich <148796+ilmich@users.noreply.github.com>
2025-09-07 20:38:01 +0200

    sc: fix server

--- servers/streamingcommunityws.py
+++ servers/streamingcommunityws.py
@@ -13,12 +13,11 @@ from platformcode import logger, config
 
 vttsupport = False if int(xbmc.getInfoLabel('System.BuildVersion').split('.')[0]) < 20 else True
 
-
 def test_video_exists(page_url):
     global iframeParams
     global urlParams
     server_url = support.scrapertools.decodeHtmlentities(support.match(page_url, patron=['<iframe [^>]+src="([^"]+)', 'embed_url="([^"]+)']).match)
-    iframeParams = support.match(server_url, patron=r'''"quality":(\d+),.+window\.masterPlaylist\s+=\s+{[^{]+({[^}]+}),\s+url:\s+'([^']+)''').match
+    iframeParams = support.match(server_url, patron=r'''window\.masterPlaylist\s+=\s+{[^{]+({[^}]+}),\s+url:\s+'([^']+).*?canPlayFHD\s=\s(true|false)''', debug=False).match
 
     if not iframeParams or len(iframeParams) < 2:
         return 'StreamingCommunity', 'Prossimamente'
@@ -30,18 +29,19 @@ def test_video_exists(page_url):
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
     video_urls = list()
 
-    quality, params, url = iframeParams
+    params, url, canPlayFHD = iframeParams
     split_url = urllib.parse.urlsplit(url)
     url_params = urllib.parse.parse_qsl(split_url.query)
     logger.debug(url_params)
     masterPlaylistParams = ast.literal_eval(params)
-    if 'canPlayFHD' in urlParams:
+    if canPlayFHD == 'true':
         masterPlaylistParams['h'] = 1
     if 'b' in urlParams:
         masterPlaylistParams['b'] = 1
+    
     masterPlaylistParams.update(url_params)
     url =  '{}://{}{}?{}'.format(split_url.scheme,split_url.netloc,split_url.path,urllib.parse.urlencode(masterPlaylistParams))
 
-    video_urls = [['hls [{}]'.format(quality), url]]
+    video_urls = [['hls [{}]'.format('FullHD' if canPlayFHD == 'true' else 'HD'), url]]
 
     return video_urls
